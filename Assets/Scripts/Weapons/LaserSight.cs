6856b65c472b37f7320dff9ae1d8bffb99b423a3
Gozilla <en.gozilla@gmail.com>
2026-09-24 18:27:57 +0300

    Lazer sight fixes

--- Assets/Scripts/Weapons/LaserSight.cs
+++ Assets/Scripts/Weapons/LaserSight.cs
@@ -7,21 +7,28 @@ public class LaserSight : MonoBehaviour
 
 	[SerializeField] private Transform muzzle;
 	[SerializeField] private float sightRange = 100;
-	private RaycastHit hit, hit1;
+	private RaycastHit[] hit = new RaycastHit[4];
 	private bool attackStart = false;
 
 	private void Update()
 	{
-		if (Input.GetKey(KeyCode.Mouse0)) Attack();
+		//if (Input.GetKey(KeyCode.Mouse0)) Attack();
+		CastRay();
 	}
 
-	private void Attack()
+	private void CastRay()
 	{
-		if (Physics.Raycast(muzzle.position, muzzle.forward, out hit, sightRange))
+		if (Physics.Raycast(muzzle.position, muzzle.forward, out hit[0], sightRange))
 		{
 			attackStart = true;
-			Debug.Log("hit " + hit.transform.name);
-
+			//Debug.Log("hit " + hit[0].transform.name);
+			Vector3 incomingDirection = hit[0].point - muzzle.position;
+			//Debug.DrawRay(
+			//		hit.point,
+			//		incomingDirection * 5f,
+			//		Color.red,
+			//		2f
+			//	);
 		}
 	}
 
@@ -31,27 +38,30 @@ public class LaserSight : MonoBehaviour
 		{
 			Gizmos.color = Color.yellow;
 			// Первая нормаль
-			Vector3 hitNormal = hit.point + hit.normal; // Нормаль от точки попадания
-			Gizmos.DrawLine(hit.point, hitNormal);
+			Vector3 hitNormal = hit[0].normal; // Нормаль от точки попадания
+			//Gizmos.DrawLine(hit[0].point, hit[0].point + hitNormal);
 			Gizmos.color = Color.red;
 			// Первый луч
-			Gizmos.DrawLine(muzzle.position, hit.point);
-			Vector3 incomingDirection = hit.point - muzzle.position; // Входящий вектор 
+			Gizmos.DrawLine(muzzle.position, hit[0].point);
+			Vector3 incomingDirection = hit[0].point - muzzle.position; // Входящий вектор 
 			// Отраженный вектор 
 			Vector3 reflectedDir = Vector3.Reflect(incomingDirection.normalized, hitNormal);
 
-
-			if (Physics.Raycast(hit.point, reflectedDir, out hit1, sightRange))
+			if (Physics.Raycast(hit[0].point, reflectedDir, out hit[1], sightRange))
 			{
-				Vector3 hitNormal1 = hit1.point + hit1.normal;
+				Vector3 hitNormal1 = hit[1].normal;
 				Gizmos.color = Color.yellow;
-				Gizmos.DrawLine(hit1.point, hitNormal1);
+				//Gizmos.DrawLine(hit[1].point, hit[1].point + hitNormal);
 				Gizmos.color = Color.red;
-				Gizmos.DrawLine(hit.point, hit1.point);
+				Gizmos.DrawLine(hit[0].point, hit[1].point);
+				Vector3 incomingDirection1 = hit[0].point - hit[1].point;
+				Vector3 reflectedDir1 = Vector3.Reflect(incomingDirection1.normalized, hitNormal1);
+				Gizmos.DrawLine(hit[1].point, hit[1].point + reflectedDir1 * sightRange);
 			}
 			else
 			{
-				Gizmos.DrawLine(hit.point, reflectedDir * 2);
+				Gizmos.DrawLine(hit[0].point, hit[0].point + reflectedDir * sightRange);
+
 			}
 		}
 	}
